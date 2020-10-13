ef6c0a4fe22b842827a9c9cb36edd085a5765bd7
darul-polcep <darulihsanbukik@gmail.com>
2020-10-13 12:00:40 +0700

    Update new

--- latihan002.py
+++ latihan002.py
@@ -1,3 +1,13 @@
 myList = ['Luke Skywalke', 'Analki Skywalker', 'Yoda', 'Padmi Amidala']
 
-print("Hello Word")
+print("myList")
+
+print("Hello my name is %s" % myList[0])
+
+print(myList[::-1])
+
+for index in myList[::-1]:
+    print(index)
+    
+modulo = 10 % 4
+print(modulo)
