25f21b43f995d1e914f5d31b997e503908b117bd
darul-polcep <darulihsanbukik@gmail.com>
2020-10-14 10:30:40 +0700

    first commit

---
+++ percobaan.py
@@ -0,0 +1,18 @@
+numbers = []
+strings = []
+names = ["Anakin Skywalker", "Adela Putri", "Han Solo", "Qui-Gon Jinn", "Luke Skywalker", "Obi-Wan Kenobi"] 
+
+# write
+second_name = None
+
+
+# this code should write out the filled arrays and the second name in the names list (Padme Amilada).
+print(numbers)
+print(strings)
+print("The second name on the names list is %s" % second_name)
+
+
+# print the list variable use for
+
+
+# print the list without name "Han Solo"
