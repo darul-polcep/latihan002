4705b4aaf5930dc220944ac238a2fe75ad004e56
darul-polcep <darulihsanbukik@gmail.com>
2020-10-15 12:00:01 +0700

    pelatihan hari ke 4

---
+++ latihan04.py
@@ -0,0 +1,62 @@
+#astring = "Hello World!"
+
+#print(astring.index("o"))
+
+#astring = "Hello World!"
+
+#print(astring.count("o"))
+
+object = "Adela Dwi Putri"
+
+#print(object.count("i"))
+#print(object.index("u"))
+
+astring = "Hello World!"
+
+alist = astring.split(" ")
+
+print(astring[3:7])
+
+print(astring[4:6:2])
+
+print(astring[::-1])
+print(astring.upper())
+print(astring.lower())
+
+#split
+
+astring = "Hello World, my name is Darul!"
+
+print("Split samplel %s " % astring.split(" "))
+
+#conditon
+x = 2
+
+if "Hello" in alist:
+    print("Yes I have...")
+    
+print(len(alist))
+
+#The "is" operator
+
+Jiwa = [1,2,3]
+Kulo = [1,2,3]
+print(Jiwa == Kulo)
+print(Jiwa is Kulo)
+
+#The "not" operator
+
+print(not False) # Prints out True
+print((not false)) == ((False)) # print out False
+ 
+
+
+
+
+
+
+
+
+
+
+
